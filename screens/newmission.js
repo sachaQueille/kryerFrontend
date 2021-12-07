4567bb74bd7a11825594c7bde7c0473777e418f4
Chaythy <chaythy@gmail.com>
2021-12-07 11:26:15 +0100

    correction bug size et mise en place de scrollview

--- screens/newmission.js
+++ screens/newmission.js
@@ -4,96 +4,97 @@ import { FontAwesome, Ionicons } from '@expo/vector-icons';
 // import { View, Text } from 'react-native';
 
 import { NativeBaseProvider, Center, HStack, Badge, Box, Button, Stack, Icon, Text, Avatar } from 'native-base';
+import { ScrollView } from "react-native-gesture-handler";
 
 
 export default function NewMission(props) {
     return (
         <NativeBaseProvider>
+            <ScrollView>
+                <Center
+                    bg="indigo.800"
+                    _text={{
+                        color: "#9b59b6",
+                        fontWeight: "600",
+                        fontSize: "32",
 
-            <Center
-                bg="indigo.800"
-                _text={{
-                    color: "#9b59b6",
-                    fontWeight: "600",
-                    fontSize: "32",
+                    }}
+                    height={160}
+                    width="100%">
 
-                }}
-                height={160}
-                width="100%">
-
-                Nouvelle mission
-            </Center>
+                    Nouvelle mission
+                </Center>
 
 
 
-            <Stack flex={1}
-                direction={{
-                    base: "column",
-                    md: "row",
-                    justifyContent: 'center',
+                <Stack flex={1}
+                    direction={{
+                        base: "column",
+                        md: "row",
+                        justifyContent: 'center',
 
-                }}
-                space={8}
-                px="10"
-                py="10"
-                width="100%"
-                height="80%"
-                flexDir="column">
-                <Button
-                    leftIcon={<Avatar
-                        bg="purple.600"
-                        alignSelf="center"
-                        size="xl"
-                        source={{
-                            uri: "./assets/avatarfemal.jpg",
-                        }}
-                    />}
-                    rightIcon={<FontAwesome name="cube" size="32" type="Ionicons" color="white" >13 kg </FontAwesome>}
-                    colorScheme="purple"
-                    onPress={() => props.navigation.navigate('NewMissionOne')}
-                >
-                    Alice DUPONT
-                    <Text> Paris/Martinique</Text>
+                    }}
+                    space={8}
+                    px="10"
+                    py="10"
+                    width="100%"
+                    height="80%"
+                    flexDir="column">
+                    <Button
+                        leftIcon={<Avatar
+                            bg="purple.600"
+                            alignSelf="center"
+                            size="xl"
+                            source={{
+                                uri: "./assets/avatarfemal.jpg",
+                            }}
+                        />}
+                        rightIcon={<FontAwesome name="cube" size={32} type="Ionicons" color="white" >13 kg </FontAwesome>}
+                        colorScheme="purple"
+                        onPress={() => props.navigation.navigate('NewMissionOne')}
+                    >
+                        Alice DUPONT
+                        <Text> Paris/Martinique</Text>
 
-                </Button>
+                    </Button>
 
-                <Button
-                    leftIcon={<Avatar
-                        bg="purple.600"
-                        alignSelf="center"
-                        size="xl"
-                        source={{
-                            uri: "./assets/avatarfemal.jpg",
-                        }}
-                    />}
-                    rightIcon={<FontAwesome name="cube" size="32" type="Ionicons" color="white" >13 kg </FontAwesome>}
-                    colorScheme="purple"
-                    onPress={() => props.navigation.navigate('NewMissionOne')}
-                >
-                    Jean LEPETIT
-                    <Text> Paris/Martinique</Text>
+                    <Button
+                        leftIcon={<Avatar
+                            bg="purple.600"
+                            alignSelf="center"
+                            size="xl"
+                            source={{
+                                uri: "./assets/avatarfemal.jpg",
+                            }}
+                        />}
+                        rightIcon={<FontAwesome name="cube" size={32} type="Ionicons" color="white" >13 kg </FontAwesome>}
+                        colorScheme="purple"
+                        onPress={() => props.navigation.navigate('NewMissionOne')}
+                    >
+                        Jean LEPETIT
+                        <Text> Paris/Martinique</Text>
 
-                </Button>
+                    </Button>
 
-                <Button
-                    leftIcon={<Avatar
-                        bg="purple.600"
-                        alignSelf="center"
-                        size="xl"
-                        source={{
-                            uri: "./assets/avatarfemal.jpg",
-                        }}
-                    />}
-                    rightIcon={<FontAwesome name="cube" size="32" type="Ionicons" color="white" >13 kg </FontAwesome>}
-                    colorScheme="purple"
-                    onPress={() => props.navigation.navigate('NewMissionOne')}
-                >
-                    Jack SPAROW
-                    <Text> Martinique/Paris</Text>
+                    <Button
+                        leftIcon={<Avatar
+                            bg="purple.600"
+                            alignSelf="center"
+                            size="xl"
+                            source={{
+                                uri: "./assets/avatarfemal.jpg",
+                            }}
+                        />}
+                        rightIcon={<FontAwesome name="cube" size={32} type="Ionicons" color="white" >13 kg </FontAwesome>}
+                        colorScheme="purple"
+                        onPress={() => props.navigation.navigate('NewMissionOne')}
+                    >
+                        Jack SPAROW
+                        <Text> Martinique/Paris</Text>
 
-                </Button>
-            </Stack>
-
+                    </Button>
+                </Stack>
+            </ScrollView>
 
 
         </NativeBaseProvider>
