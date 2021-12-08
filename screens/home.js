5e4f9b0f95eeddfe5ed217b416d4f5b286bee0f4
Chaythy <chaythy@gmail.com>
2021-12-08 09:24:26 +0100

    traitement partiel sur newMission et MAJ newMissionOne de la card

--- screens/home.js
+++ screens/home.js
@@ -3,35 +3,35 @@ import { View, Text } from "react-native";
 import { Button, NativeBaseProvider, VStack } from "native-base";
 
 export default function Home(props) {
-  return (
-    <NativeBaseProvider>
-      <VStack
-        mx="auto"
-        marginTop={80}
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>KRYER</Text>
+    return (
+        <NativeBaseProvider>
+            <VStack
+                mx="auto"
+                marginTop={80}
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>KRYER</Text>
 
-        <Text>Envoyez vos colis à l'international à moindre frais</Text>
-        <Button
-          colorScheme="indigo"
-          onPress={() => props.navigation.navigate("PurposeJourney")}
-        >
-          Proposer une mission
-        </Button>
+                <Text>Envoyez vos colis à l'international à moindre frais</Text>
+                <Button
+                    colorScheme="indigo"
+                    onPress={() => props.navigation.navigate("PurposeJourney")}
+                >
+                    Proposer une mission
+                </Button>
 
-        <View>
-          <Button
-            colorScheme="indigo"
-            onPress={() => props.navigation.navigate("SendDelivery")}
-          >
-            Envoyer colis
-          </Button>
-        </View>
-      </VStack>
-    </NativeBaseProvider>
-  );
+                <View>
+                    <Button
+                        colorScheme="indigo"
+                        onPress={() => props.navigation.navigate("SendDelivery")}
+                    >
+                        Envoyer colis
+                    </Button>
+                </View>
+            </VStack>
+        </NativeBaseProvider>
+    );
 }
 
 
