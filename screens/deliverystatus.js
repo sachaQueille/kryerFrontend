7ff234f99d29cfd94026c51cf9a9e5f83ffe1fc5
Chaythy <chaythy@gmail.com>
2021-12-15 15:16:46 +0100

    deliverystatus finalisé et style général

--- screens/deliverystatus.js
+++ screens/deliverystatus.js
@@ -1,225 +1,210 @@
 import React, { useState, useEffect } from "react";
 import {
-    NativeBaseProvider,
-    Center,
-    Box,
-    Button,
-    VStack,
-    Heading,
-    Progress,
-    Checkbox,
-    Text,
-    Modal,
-} from 'native-base';
-
+  NativeBaseProvider,
+  Center,
+  Box,
+  Button,
+  VStack,
+  Heading,
+  Progress,
+  Checkbox,
+  Text,
+  Modal,
+} from "native-base";
 
 function DeliveryStatus(props) {
+  var statusDelivery = props.route.params.deliveryStatus.delivery_status;
+  var verifcode = props.route.params.deliveryStatus.verifcode;
+  var price = props.route.params.deliveryStatus.price;
+  const [supportedDelivery, setSupportedDelivery] = useState(false);
+  const [inTransitDelivery, setIntransitDelivery] = useState(false);
+  const [delivered, setDelivered] = useState(false);
+  const [inProgress, setInProgress] = useState(0);
+  const [disableButton, setDisableButton] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [existDelivery, setExistDelivery] = useState(true);
+  const [message, setMessage] = useState(true);
 
-    var statusDelivery = props.route.params.deliveryStatus.delivery_status;
-    var verifcode = props.route.params.deliveryStatus.verifcode
-var price = props.route.params.deliveryStatus.price
-    const [supportedDelivery, setSupportedDelivery] = useState(false);
-    const [inTransitDelivery, setIntransitDelivery] = useState(false);
-    const [delivered, setDelivered] = useState(false);
-    const [inProgress, setInProgress] = useState(0);
-    const [disableButton, setDisableButton] = useState(true);
-    const [showModal, setShowModal] = useState(false);
-    const [existDelivery, setExistDelivery] = useState(true)
-    const [message, setMessage] = useState(true)
-
-
-    useEffect(() => {
-        if (statusDelivery === 'supportedDelivery') {
-            setSupportedDelivery(true);
-            setInProgress(33);            
-        }
-        if (statusDelivery === 'inTransitDelivery') {
-            setSupportedDelivery(true);
-            setIntransitDelivery(true);
-            setInProgress(66);
-            setDisableButton(false);
-        }
-        if (statusDelivery === 'delivered') {
-            setSupportedDelivery(true);
-            setIntransitDelivery(true);
-            setDelivered(true)
-            setInProgress(100);
-            setDisableButton(false);
-            setMessage(false);
-        }
-    }, []);
-
-    async function deleteMyDelivery(verifcode) {
-
-        var response = await fetch(`${global.ipa}deleteMyDelivery/${verifcode}`, {
-            method: 'DELETE',
-        });
-        response = await response.json()
-
-        if (response) {
-            setShowModal(true);
-            setExistDelivery(false);
-        }
+  useEffect(() => {
+    if (statusDelivery === "supportedDelivery") {
+      setSupportedDelivery(true);
+      setInProgress(33);
     }
+    if (statusDelivery === "inTransitDelivery") {
+      setSupportedDelivery(true);
+      setIntransitDelivery(true);
+      setInProgress(66);
+      setDisableButton(false);
+    }
+    if (statusDelivery === "delivered") {
+      setSupportedDelivery(true);
+      setIntransitDelivery(true);
+      setDelivered(true);
+      setInProgress(100);
+      setDisableButton(false);
+      setMessage(false);
+    }
+  }, []);
 
-    console.log("props", props.route.params.deliveryStatus);
+  async function deleteMyDelivery(verifcode) {
+    var response = await fetch(`${global.ipa}deleteMyDelivery/${verifcode}`, {
+      method: "DELETE",
+    });
+    response = await response.json();
 
+    if (response) {
+      setShowModal(true);
+      setExistDelivery(false);
+    }
+  }
 
+  console.log("props", props.route.params.deliveryStatus);
 
-    if (existDelivery) {
-        return (
-            <NativeBaseProvider>
+  if (existDelivery) {
+    return (
+      <NativeBaseProvider>
+        <Center
+          style={{ backgroundColor: "indigo" }}
+          _text={{
+            color: "#ffffff",
+            fontWeight: "600",
+            fontSize: "32",
+            marginTop: "10%",
+          }}
+          height={120}
+          width="100%"
+        >
+          Suivre mon colis
+        </Center>
 
-                <Center
-                    style={{ backgroundColor: "indigo" }}
-                    _text={{
-                        color: "#ffffff",
-                        fontWeight: "600",
-                        fontSize: "32",
-                        marginTop: "10%"
+        <Center marginTop="60">
+          <Box w="90%">
+            <VStack space="md">
+              <Heading textAlign="center" mb="10" size="md">
+                Etat de la livraison
+              </Heading>
+              <VStack mx="4" space="md">
+                <Progress size="2xl" colorScheme="purple" value={inProgress} />
+              </VStack>
+            </VStack>
+          </Box>
+        </Center>
 
-                    }}
-                    height={120}
-                    width="100%">
-                    Suivre mon colis
-                </Center>
+        <Center marginTop="100">
+          <VStack space={3}>
+            <Checkbox
+              colorScheme="purple"
+              size="lg"
+              isChecked={supportedDelivery}
+            >
+              Colis pris en charge
+            </Checkbox>
+            <Checkbox
+              colorScheme="purple"
+              size="lg"
+              isChecked={inTransitDelivery}
+            >
+              Colis en transit
+            </Checkbox>
+            <Checkbox colorScheme="purple" size="lg" isChecked={delivered}>
+              Colis livré
+            </Checkbox>
+          </VStack>
+        </Center>
 
-                <Center marginTop="60">
-                    <Box w="90%">
-                        <VStack space="md">
-                            <Heading textAlign="center" mb="10" size="md">
-                                Etat de la livraison
-                            </Heading>
-                            <VStack mx="4" space="md">
-                                <Progress size="2xl" colorScheme="purple" value={inProgress} />
+        <Center width="80%" marginTop="20%" marginLeft="10%">
+          <Text>
+            {message
+              ? `Veuillez indiquer ce code à votre personne de confiance qui réceptionnera votre colis :`
+              : `Votre colis est bien livré, vous allez être débité de ${price}€ de votre compte KRYER. Votre code de vérification est :`}
+          </Text>
+          <Text marginTop="5%" fontWeight="bold">
+            {verifcode}
+          </Text>
+        </Center>
 
-                            </VStack>
-                        </VStack>
-                    </Box>
-                </Center>
-
-                <Center marginTop="100">
-                    <VStack space={3}>
-                        <Checkbox
-                            colorScheme="purple"
-                            size="lg"
-                            isChecked={supportedDelivery}
-                        >
-                            Colis pris en charge
-                        </Checkbox>
-                        <Checkbox
-                            colorScheme="purple"
-                            size="lg"
-                            isChecked={inTransitDelivery}
-                        >
-                            Colis en transit
-                        </Checkbox>
-                        <Checkbox
-                            colorScheme="purple"
-                            size="lg"
-                            isChecked={delivered}
-                        >
-                            Colis livré
-                        </Checkbox>
-                    </VStack>
-                </Center>
-
-                <Center
-                width="80%"
-                    marginTop="20%"
-                    marginLeft="10%" >
-                    <Text>
-                        {message ?
-                            `Veuillez indiquer ce code à votre personne de confiance qui réceptionnera votre colis :` :
-                            `Votre colis est bien livré, vous allez être débité de ${price}€ de votre compte KRYER. Votre code de vérification est :`}
-                    </Text>
-                    <Text marginTop="5%" fontWeight="bold">
-                    {verifcode}
-                    </Text>
-                </Center>
-
-                <Center marginTop="100">
-                    <VStack space={4} alignItems="center">
-                        <Box>
-                            {disableButton ? <Button size="lg"
-                                backgroundColor="error.800"
-                                onPress={() => { deleteMyDelivery(verifcode) }
-                                }>
-                                Annuler la demande
-                            </Button> : null}
-                        </Box>
-                    </VStack>
-                </Center>
-
-                <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-                    <Modal.Content maxWidth="400px">
-                        <Modal.CloseButton />
-                        <Modal.Header>Colis supprimé</Modal.Header>
-                        <Modal.Body>
-                            <Text>Vous avez bien annulé votre demande</Text>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button
-                                variant="ghost"
-                                colorScheme="blueGray"
-                                onPress={() => {
-                                    setShowModal(false);
-                                }}
-                            >
-                                OK
-                            </Button>
-                        </Modal.Footer>
-                    </Modal.Content>
-                </Modal>
-
-            </NativeBaseProvider>
-        );
-
-    } else {
-        return (
-            <NativeBaseProvider>
-                <Center
-                    style={{ backgroundColor: "indigo" }}
-                    _text={{
-                        color: "#ffffff",
-                        fontWeight: "600",
-                        fontSize: "32",
-                        marginTop: "10%",
-                    }}
-                    height={120}
-                    width="100%"
+        <Center marginTop="100">
+          <VStack space={4} alignItems="center">
+            <Box>
+              {disableButton ? (
+                <Button
+                  size="lg"
+                  backgroundColor="error.800"
+                  onPress={() => {
+                    deleteMyDelivery(verifcode);
+                  }}
                 >
-                    Nouvelles missions
-                </Center>
-                <Center marginTop="50%">
-                    <Text>
-                        Vous n'avez plus d'informations pour ce colis
-                    </Text>
-                </Center>
-                <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-                    <Modal.Content maxWidth="400px">
-                        <Modal.CloseButton />
-                        <Modal.Header>Colis supprimé</Modal.Header>
-                        <Modal.Body>
-                            <Text>Vous avez bien annulé votre demande</Text>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button
-                                variant="ghost"
-                                colorScheme="blueGray"
-                                onPress={() => {
-                                    setShowModal(false);
-                                }}
-                            >
-                                OK
-                            </Button>
-                        </Modal.Footer>
-                    </Modal.Content>
-                </Modal>
-            </NativeBaseProvider>
-        );
-    }
+                  Annuler la demande
+                </Button>
+              ) : null}
+            </Box>
+          </VStack>
+        </Center>
+
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Header>Colis supprimé</Modal.Header>
+            <Modal.Body>
+              <Text>Vous avez bien annulé votre demande</Text>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setShowModal(false);
+                }}
+              >
+                OK
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </NativeBaseProvider>
+    );
+  } else {
+    return (
+      <NativeBaseProvider>
+        <Center
+          style={{ backgroundColor: "indigo" }}
+          _text={{
+            color: "#ffffff",
+            fontWeight: "600",
+            fontSize: "32",
+            marginTop: "10%",
+          }}
+          height={120}
+          width="100%"
+        >
+          Nouvelles missions
+        </Center>
+        <Center marginTop="50%">
+          <Text>Vous n'avez plus d'informations pour ce colis</Text>
+        </Center>
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Header>Colis supprimé</Modal.Header>
+            <Modal.Body>
+              <Text>Vous avez bien annulé votre demande</Text>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setShowModal(false);
+                }}
+              >
+                OK
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </NativeBaseProvider>
+    );
+  }
 }
 
-
-export default DeliveryStatus;  
+export default DeliveryStatus;
--- screens/tchatdetails.js
+++ screens/tchatdetails.js
@@ -1,49 +1,46 @@
-import React, {useEffect, useState} from "react"
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import {
-    Box,
-    FlatList,
-    Icon,
-    Center,
-    HStack,
-    VStack,
-    Text,
-    Spacer,
-    Button,
-    NativeBaseProvider,
-    Input
-  } from "native-base";
+  Box,
+  FlatList,
+  Icon,
+  Center,
+  HStack,
+  VStack,
+  Text,
+  Spacer,
+  Button,
+  NativeBaseProvider,
+  Input,
+} from "native-base";
 
-  import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { ScrollView } from "react-native-gesture-handler";
 
 function TchatDetails(props) {
-    console.log(props.route.params);
+  console.log(props.route.params);
 
-    //on va chercher tous les messages en lien avec cet utilisateur
+  //on va chercher tous les messages en lien avec cet utilisateur
 
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [dataMessages, setDataMessages] = useState();
 
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [dataMessages, setDataMessages] = useState();
+  useEffect(() => {
+    async function loadMessages() {
+      var response = await fetch("http://192.168.1.109:3000/loadMessages", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `token=${props.user.token}&idRecipient=${props.route.params.idRecipient}`,
+      });
 
-    useEffect(() => {
+      response = await response.json();
+      setDataMessages(response.messages);
+      console.log("response", response.messages);
+    }
+    loadMessages();
+  }, []);
 
-        async function loadMessages() {
-            var response = await fetch("http://192.168.1.109:3000/loadMessages", {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: `token=${props.user.token}&idRecipient=${props.route.params.idRecipient}`
-            });
-
-            response = await response.json();
-            setDataMessages(response.messages);
-            console.log("response", response.messages);
-        }
-        loadMessages()
-        
-    }, []);
-
-    const data = [
+  const data = [
     {
       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
       id2: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
@@ -88,127 +85,122 @@ function TchatDetails(props) {
       avatarUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
     },
-  ] 
+  ];
 
   var propertyJusifyContent = "";
-  function flexMessageProperty(userId){
-      if(userId === "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba"){
-          propertyJusifyContent = "flex-end"; 
-      } else {
-        propertyJusifyContent = "flex-start";
-      }
-      return propertyJusifyContent;
+  function flexMessageProperty(userId) {
+    if (userId === "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba") {
+      propertyJusifyContent = "flex-end";
+    } else {
+      propertyJusifyContent = "flex-start";
+    }
+    return propertyJusifyContent;
   }
 
-    return (
-        <NativeBaseProvider >
-            <Center
-                style={{ backgroundColor: "indigo" }}
-                _text={{
-                    color: "#ffffff",
-                    fontWeight: "600",
-                    fontSize: "32",
-                    marginTop: "10%"
-
-                }}
-                height={120}
-                width="100%">
-                Name
-            </Center>
+  return (
+    <NativeBaseProvider>
+      <Center
+        style={{ backgroundColor: "indigo" }}
+        _text={{
+          color: "#ffffff",
+          fontWeight: "600",
+          fontSize: "32",
+          marginTop: "10%",
+        }}
+        height={120}
+        width="100%"
+      >
+        Name
+      </Center>
+      <Box
+        w={{
+          base: "100%",
+          md: "25%",
+        }}
+      >
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
             <Box
-            w={{
-                base: "100%",
-                md: "25%",
-            }}
+              borderBottomWidth="1"
+              _dark={{
+                borderColor: "gray.600",
+              }}
+              borderColor="coolGray.200"
+              pl="4"
+              pr="5"
+              py="2"
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: flexMessageProperty(item.id2),
+              }}
             >
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                    <Box
-                        borderBottomWidth="1"
-                        _dark={{
-                        borderColor: "gray.600",
-                        }}
-                        borderColor="coolGray.200"
-                        pl="4"
-                        pr="5"
-                        py="2"
-                        style={{flex:1, flexDirection:'row',justifyContent:flexMessageProperty(item.id2)}}
-                    >
-                        <HStack space={3} justifyContent="space-between">
-                        
-                        <VStack>
-                            <Text
-                            
-                            _dark={{
-                                color: "warmGray.50",
-                            }}
-                            color="coolGray.800"
-                            bold
-                            >
-                            {item.recentText}
-                            </Text>
-
-                            <Text
-                            style={{textAlign: 'left'}}
-                            fontSize="xs"
-                            _dark={{
-                            color: "warmGray.50",
-                            }}
-                            color="coolGray.800"
-                            alignSelf="flex-start"
-                            >
-                            {item.timeStamp}
-                            </Text>
-                        </VStack>
-                        <Spacer />
-                        
-                        </HStack>
-                    </Box>
-
-                    )}
-                    keyExtractor={(item) => item.id_dest}
-                />
-            </Box>
-
-            <VStack style = {{position: 'absolute', left: 0, right: 0, bottom: 0}}>
-                
-                <Input
-                    onChangeText={(e) => setCurrentMessage(e)}
-                    value={currentMessage}
-                    w={{
-                    base: "100%",
-                    md: "25%",
+              <HStack space={3} justifyContent="space-between">
+                <VStack>
+                  <Text
+                    _dark={{
+                      color: "warmGray.50",
                     }}
-                    InputRightElement={
-                    <Button
-                    onPress={()=> {
-                        /* socket.emit("sendMessage", {msg: currentMessage, pseudo: props.pseudo}) */
-                        setCurrentMessage('');
-                      } } 
-                    style={{backgroundColor:"transparent"}}
-                    >
-                        <Icon
-                            as={<MaterialCommunityIcons name="send" />}
-                            size={7}
-                            mr="2"
-                            color="muted.400"
-                        />
-                    </Button>
-                    }
-                    placeholder="Ecrire un message"
-                />
-            </VStack>
-        </NativeBaseProvider>
-    );
-}
+                    color="coolGray.800"
+                    bold
+                  >
+                    {item.recentText}
+                  </Text>
 
+                  <Text
+                    style={{ textAlign: "left" }}
+                    fontSize="xs"
+                    _dark={{
+                      color: "warmGray.50",
+                    }}
+                    color="coolGray.800"
+                    alignSelf="flex-start"
+                  >
+                    {item.timeStamp}
+                  </Text>
+                </VStack>
+                <Spacer />
+              </HStack>
+            </Box>
+          )}
+          keyExtractor={(item) => item.id_dest}
+        />
+      </Box>
+
+      <VStack style={{ position: "absolute", left: 0, right: 0, bottom: 0 }}>
+        <Input
+          onChangeText={(e) => setCurrentMessage(e)}
+          value={currentMessage}
+          w={{
+            base: "100%",
+            md: "25%",
+          }}
+          InputRightElement={
+            <Button
+              onPress={() => {
+                /* socket.emit("sendMessage", {msg: currentMessage, pseudo: props.pseudo}) */
+                setCurrentMessage("");
+              }}
+              style={{ backgroundColor: "transparent" }}
+            >
+              <Icon
+                as={<MaterialCommunityIcons name="send" />}
+                size={7}
+                mr="2"
+                color="muted.400"
+              />
+            </Button>
+          }
+          placeholder="Ecrire un message"
+        />
+      </VStack>
+    </NativeBaseProvider>
+  );
+}
 
 function mapStateToProps(state) {
-    return { user: state.userReducer }
+  return { user: state.userReducer };
 }
 
-export default connect(
-    mapStateToProps,
-    null
-)(TchatDetails);
+export default connect(mapStateToProps, null)(TchatDetails);
